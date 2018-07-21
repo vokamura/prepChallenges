678b44b1f1f696f9e906e4696d1c1f2ac72e1cb5
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-07-21 05:56:20 -0700

    added problems

--- challenges.js
+++ challenges.js
@@ -1,6 +1,69 @@
+/* WEEK 1 */
+/*
+FIND LARGEST NUMBER
+task: make a function that determines which number is largest
+input: 4, 12, 2
+output: 12
+*/
+fuction findLargestNumber( num1, num2, num3 ){
+	
+}
+/*
+task: make a function that, given 3 strings, returns which string is longest
+input: 'stuff', 'hi', 'woohoo'
+output: 'woohoo'
+*/
+function findLongestWord( word1, word2, word3){
+
+}
 
 /*
-DAY 1
+DETERMINE EVEN DIBIBILITY
+task: make a function that, given 2 numbers, determines if the 1st number
+	is evenly divisible by the 2nd number
+----
+input: 10,2
+output: true
+----
+input: 5, 2
+output: false
+*/
+function determineEvenDivisibility( num1, num2 ){
+	
+}
+
+/*
+CHECK FIRST LETTER CAPITLIZED
+task: make a function that, given a string, determines if the 1st letter is capitalized or not
+----
+input: 'Fruit'
+output: true
+----
+input: 'fruit'
+output: false
+*/
+function checkFirstLetterCapitalized( word ){
+
+}
+
+/*
+CHECK OUTSIDE LETTERS
+task: make a function that determines if the first and last letter of a string are the same letter
+----
+input: 'sisters',
+output: true
+----
+input: 'mister',
+output: false
+/*
+function checkOutsideLetters( word ){
+	
+}
+
+
+/* WEEK 2 */
+/*
+REVERSE STRING
 task: make a fuction that uses loops to reverse a string
 input: 'greetings earthlings'
 output: 'sgnilhtrae sgniteerg' 
@@ -10,7 +73,7 @@ function reverseString( string ){
 }
 
 /*
-DAY 2
+CALCULATE FACTORIAL
 task: calculate the factorial of a number
 	factorial is a number multiplied by all the numbers lower than it, down to 1
 input: 5
@@ -21,11 +84,42 @@ function calculateFactorial( number ){
 }
 
 /*
-DAY 3
+FIND LONGEST WORD IN ARRAY
 task: given an array of words, determine which one is the longest and returns it
 input: ['great','days','hi']
 output: 'great'
 */
-function findLongestWord( wordArray ){
-	
+function findLongestWordInArray( wordArray ){
+
 }
+
+/*
+EVERY OTHER LETTER
+task: given a string, return a string with every other letter
+----
+input: 'silliness'
+output: 'ilns'
+----
+input: 'mistermister'
+output: 'itritr'
+*/
+function getEveryOtherLetter( string ){
+
+}
+
+/*
+ALPHABETIZE LETTERS
+task: given a string, return a string with each letter in alphabetical order
+----
+input: 'marshmallow'
+output: 'aahllmmorsw'
+----
+input: 'tuba'
+output: 'abtu'
+*/
+function alphabetizeLetters( string ){
+
+}
+
+
+
