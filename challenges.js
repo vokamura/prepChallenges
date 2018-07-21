896ef05202fdf4a82ff08539aa6a97d23f7fa3bc
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-07-21 05:31:11 -0700

    tests

---
+++ challenges.js
@@ -0,0 +1,31 @@
+
+/*
+DAY 1
+task: make a fuction that uses loops to reverse a string
+input: 'greetings earthlings'
+output: 'sgnilhtrae sgniteerg' 
+*/
+function reverseString( string ){
+
+}
+
+/*
+DAY 2
+task: calculate the factorial of a number
+	factorial is a number multiplied by all the numbers lower than it, down to 1
+input: 5
+output: 120   (5*4*3*2*1)
+*/
+function calculateFactorial( number ){
+
+}
+
+/*
+DAY 3
+task: given an array of words, determine which one is the longest and returns it
+input: ['great','days','hi']
+output: 'great'
+*/
+function findLongestWord( wordArray ){
+	
+}
